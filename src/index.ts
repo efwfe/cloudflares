5c0965e19a9740ab094dc717f4be907bde13298a
david <david@Davids.local>
2025-05-18 17:18:42 +0800

    add three simple endpoints of api

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,7 @@
 import { Router } from "itty-router";
 import getImages from "./handlers/get_images";
 import createImage from "./handlers/create_image";
-import getSingleImage
- from "./handlers/get_single_image";
+import getSingleImage from "./handlers/get_single_image";
 const router = Router();
 router.get("/images", getImages)
 	.post("/images", createImage)
